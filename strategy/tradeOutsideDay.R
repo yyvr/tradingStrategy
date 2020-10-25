207b1270d875f9960b1712b0b4e3bf9022a11ab0
Yang Yang <yang@Yangs-MacBook-Pro.local>
2020-10-25 21:05:30 +0200

    modified

--- strategy/tradeOutsideDay.R
+++ strategy/tradeOutsideDay.R
@@ -1,51 +1,53 @@
-tradeOutsideDay <- function(stock, start)
+# buy if next day range covers previous day, and down trend
+findOutsideDay <- function(stock)
 {
-  buy <- NULL
-  sell <- NULL
-  selldate <- NULL
-  stopLossRate <- 0.8 
-  for (i in (start + 1) : (nrow(stock) - 2))
+  outside <- NULL
+  for (i in 3 : (nrow(stock) - 2))
   {
     if (stock$High[i] > stock$High[i - 1] &
         stock$Low[i] < stock$Low[i - 1] &
         stock$Close[i] < stock$Close[i - 1] &
-        stock$Close[i] < stock$Close[i - start] #&  confirm down trend
-        #stock$Close[i] > stock$Open[i + 1]
-        )
+        stock$Close[i - 1] < stock$Close[i - 2])
     {
-      buy <- rbind(buy, stock[i + 1, ])
-      bp <- stock$Open[i + 1]
-
-      found <- 0
-      for (j in (i + 2) : nrow(stock))
-      {
-        if (bp * stopLossRate > stock$Low[j])
-        {
-          if (bp * stopLossRate < stock$Close[j])
-            sell <- c(sell, bp * stopLossRate)
-          else
-            sell <- c(sell, stock$Close[j])
-          found <- 1
-          break
-        }
-        else if (stock$Close[j] > 1.015 * bp)
-        {
-          found <- 1
-          sell <- c(sell, stock$Close[j])
-          break
-        }
-      }
-      if (found == 0)
-        sell <- c(sell, stock$Close[j])
-      selldate <- c(selldate, as.character(stock$Date[j]))
+      outside <- c(outside, i + 1)
     }
+  }
+  return (outside)
+}
+
+tradeOutsideDay <- function(stock, start)
+{
+  outside <- findOutsideDay(stock)
+  buy <- stock[outside, ]
+  sell <- NULL
+  selldate <- NULL
+  stopLossRate <- 0.9 
+  
+  for (i in outside)
+  {
+    bp <- stock$Open[i]
+    for (j in (i + 1) : ((nrow(stock) - 1)))
+    {
+      if (bp * stopLossRate > stock$Close[j])
+      {
+        break
+      }
+      else if (stock$Close[j] > 1.015 * bp)
+      {
+        break
+      }
+    }
+    
+    sell <- c(sell, stock$Close[j])
+    selldate <- c(selldate, as.character(stock$Date[j]))
   }
   trades <- data.frame(buy$Date, buy$Open, 
                        as.Date(selldate), sell, sell/buy$Open - 1, 
                        as.Date(selldate) - buy$Date)
+
   if (nrow(trades) != 0)
-    colnames(trades) <- c('buyDate', 'buyOpenPrice', 
-                       'sellDate', 'sellClosePrice', 'roc', 'interval')
+    colnames(trades) <- c('buyDate', 'buyPrice', 
+                       'sellDate', 'sellPrice', 'roc', 'interval')
   return (trades)
 }
 
@@ -56,10 +58,10 @@ num_stocks <- length(file_list)
 
 all <- NULL
 meta <- NULL
-interval <- 20
+interval <- 30
 for (i in 1 : num_stocks)
 {
-  #if (file_list[i] != 'UPM.HE.txt') next
+  #if (file_list[i] != 'CGCBV.HE.txt') next
   if (file_list[i] == '^OMXH25.txt') next
   stock <- read.csv(file_list[i])
   stock$Date <- as.Date(stock$Date)
@@ -84,4 +86,4 @@ for (i in 1 : num_stocks)
 colnames(meta) <- c('stock', 'totalRet', 'numLossTrades', 'numTotalTrades', 
                     'lossRatio', 'loss', 'gain', 'buyHoldRet', 'averageHoldingTime')
 
-
+write.csv(all, '/Users/yang/Downloads/invest/outsideday.csv')
