1df089b2b156aaee5b70f91f28544b33f7645867
Yang Yang <yang@Yangs-MacBook-Pro.local>
2022-04-17 22:34:15 +0300

    add new highs/lows index

--- strategy/marketHealth/newHighsIndex.R
+++ strategy/marketHealth/newHighsIndex.R
@@ -5,27 +5,22 @@ library(dplyr)
 library(data.table)
 library(tibble)
 
-files <- list.files(path='/Users/yang/Downloads/invest/usprices/', pattern=NULL, all.files=FALSE,
+files <- list.files(path='/Users/yang/Downloads/invest/usprices2016/', pattern=NULL, all.files=FALSE,
                     full.names=TRUE)
-start <- '2020-05-01'
-end <- '2022-04-15'
 
-ee <- as.Date('2020-12-03')
+end <- as.character(Sys.Date())
+start <- '2016-01-01'
+spy <- tq_get('SPY', from = start, to = end, get = "stock.prices")
 counters <- NULL
-period <- as.Date(end) -ee
-for (i in 1 : period) {
-  counters <- rbind(counters, c(as.character(ee), 0))
-  ee <- ee + 1
-}
-counters <- data.frame(counters)
-colnames(counters) <- c('date', 'newHigh')
-counters$newHigh <- as.numeric(counters$newHigh)
-for ( i in 1:length(files) ) {
-  print(i)
+counters <- data.frame(spy$date, 0, 0)
+colnames(counters) <- c('date', 'newHigh', 'newLow')
+
+for ( i in 1: length(files)) { #
   stock <- read.csv(files[i])
   tail <- nrow(stock)
-  if (is.null(stock) || tail == 0 || tail < 210) next
+  if (is.null(stock) | tail == 0 | tail < 210) next
   
+  print(i)
   index <- 0
   for (k in 1 : nrow(counters)) {
     index <- which(stock$date == as.Date(counters$date[k]))
@@ -34,21 +29,41 @@ for ( i in 1:length(files) ) {
       break
   }
   
-  if (index == 0) next
+  if (index == 0 | length(index) == 0) next
   for(j in index : tail) {
-    if (stock$close[j - 10] < max(stock$close[(j - 210) : (j - 10)]))
-      next
+    if (stock$close[j - 10] >= max(stock$close[(j - 210) : (j - 10)]) &
+        stock$close[j - 10] < stock$close[j]) {
+      id <- which(stock$date[j] == as.Date(counters$date))
+      counters$newHigh[id] <- counters$newHigh[id] + 1
+    }
     
-    if (stock$close[j - 10] > stock$close[j])
-      next
-    
-    id <- which(stock$date[j] == as.Date(counters$date))
-    counters$newHigh[id] <- counters$newHigh[id] + 1
+    if (stock$close[j - 10] <= min(stock$close[(j - 210) : (j - 10)]) &
+        stock$close[j - 10] > stock$close[j]) {
+      id <- which(stock$date[j] == as.Date(counters$date))
+      counters$newLow[id] <- counters$newLow[id] + 1
+    }
   }
 }
 
-plot(counters$newHigh, type = 'l', xaxt = 'n')
-axis(1, at=1:nrow(counters), labels = counters$date, cex.axis=0.6)
+counters$diff <- counters$newHigh - counters$newLow
+write.csv(counters, file = '/Users/yang/Downloads/invest/newHighsIndexSince2016.csv')
+
+spy$ma150 <- SMA(spy$close, 150)
+spy$ma60 <- SMA(spy$close, 60)
+
+ss <- 1
+ee <- nrow(spy)
+plot(spy$close[ss : ee], type = 'l', col = 'red', axes = FALSE,xlab = "", ylab = "")
+axis(side = 4, at = pretty(range(spy$close)))
+points(spy$ma60[ss: ee], type = 'l', col = 'blue', xaxt = "n")
+points(spy$ma150[ss : ee], type = 'l', col = 'green', xaxt = "n")
+par(new = TRUE) 
+plot(counters$diff[ss : ee], type = 'l', xaxt = "n")
+abline(h=100, col = 'blue')
+axis(1, at=1:(ee - ss + 1), labels = spy$date[ss : ee], cex.axis=0.6)
+
+hist(counters$diff, breaks = 30, xaxt = 'n')
+axis(side=1, at=seq(-1550,900, 50), labels=seq(-1550,900, 50))
 
 newHighs <- function() {
   counter <- 0
@@ -71,13 +86,4 @@ newHighs <- function() {
 
 cc <- newHighs()
 
-ee <- as.Date('2022-01-03')
-meta <- NULL
-counters <- NULL
-for (i in 1 : 10) {
-  counters <- rbind(counters, c(as.character(ee), 0))
-  cc <- newHighs(start, ee)
-  meta <- rbind(meta, c(as.character(ee), cc))
-  ee <- ee + 1
-}
 
