21a7e0da70af911f767a315ecb3323717af24a13
Yang Yang <yang@Yangs-MacBook-Pro.local>
2022-11-11 10:48:25 +0200

    some updates

--- strategy/downloadStockPricesFromYahoo.R
+++ strategy/downloadStockPricesFromYahoo.R
@@ -1,38 +1,41 @@
-library(tidyquant)
+library(quantmod)
 library(timetk)
 library(formattable)
 library(dplyr)
 library(data.table)
 library(tibble)
+library(tidyquant)
 
-all <- read.csv('/Users/yang/Downloads/invest/all.csv')
+all <- read.csv('/Users/yang/Downloads/invest/tickers/nasdaq_screener_1667650603297.csv')
 all <- data.frame(all)
-all <- all[all$Market.Cap > 1000000000/2, ]
 all <- all[!is.na(all$Symbol), ]
 all$Sector <- as.factor(all$Sector)
 all$Industry <- as.factor(all$Industry)
-
-tickers <- all$Symbol[(217+443+250+22+394+426+288+357+1147):nrow(all)]
-
+all <- all[all$Market.Cap > 1000000000, ]
+tickers <- all$Symbol[(1):nrow(all)]
 start <- '2020-01-01'
-end <- '2022-03-24'
+end <- as.character(Sys.Date())
 peirod <- paste(start, end)
-# length(tickers)
-
 meta <- NULL
 for ( i in 1:length(tickers)) {
   if (is.na(tickers[i]))
     next
-  print(i)
+  
   tickers[i] <- trimws(tickers[i], whitespace = "[\\h\\v]")
   stock <- tq_get(tickers[i],                    
                   from = start,
                   to = end,
                   get = "stock.prices")
-  stock$ret <- c(0, round(diff(stock$close)/stock$close[2 : nrow(stock)], 4))
   if (is.null(stock)) next
-  if (nrow(stock) < 250) next
+  #if (nrow(stock) < 250) next
   if (sum(is.na(stock), na.rm = TRUE) != 0) next
   if (any(is.na(stock$close))) next
+  print(c(i, tickers[i]))
   write.csv(stock, paste('/Users/yang/Downloads/invest/usprices/', tickers[i], sep=''))
 }
+
+#symbols <- stockSymbols(exchange = c("AMEX", "NASDAQ", "NYSE"), 
+#                        sort.by = c("Exchange", "Symbol"),
+#                        quiet = FALSE)
+
+#nyse <- symbols[symbols$Exchange == 'NYSE', ]
